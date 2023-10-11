40ce7866a63b2757bd357d08bb2fe563d5cac20f
Ionut1115 <63416808+Ionut1115@users.noreply.github.com>
2023-10-11 20:22:40 +0200

    Update main.tf

--- quickstart/101-vm-with-infrastructure/main.tf
+++ quickstart/101-vm-with-infrastructure/main.tf
@@ -88,44 +88,51 @@ resource "azurerm_storage_account" "my_storage_account" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-data "azurerm_client_config" "current" {}
+encryption {
+    services {
+      blob = {
+        enabled = true
+        key_type = "Account"
+        key_source = "Microsoft.Storage"
+      }
+      file = {
+        enabled = true
+        key_type = "Account"
+        key_source = "Microsoft.Storage"
+      }
+      table = {
+        enabled = true
+        key_type = "Account"
+        key_source = "Microsoft.Storage"
+      }
+      queue = {
+        enabled = true
+        key_type = "Account"
+        key_source = "Microsoft.Storage"
+      }
+    }
 
-resource "azurerm_key_vault" "example" {
-  name                = "examplekv"
-  location            = "location"
-  resource_group_name = "group"
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
+    key_vault_properties {
+      key_vault_uri = azurerm_key_vault.example.vault_uri
+      key_name      = "your-key-name" # Replace with your key name in Key Vault
+      key_version   = ""               # You can specify a version or leave it empty for the latest
+    }
+  }
 
-  purge_protection_enabled = true
-}
-
-resource "azurerm_key_vault_key" "example" {
-  name         = "tfex-key"
-  key_vault_id = azurerm_key_vault.example.id
-  key_type     = "RSA"
-  key_size     = 2048
-  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
-}
-
-
-resource "azurerm_storage_account" "storage_account_good_1" {
-  name                     = "examplestor"
-  resource_group_name      = "group"
-  location                 = "location"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  identity {
-    type = "SystemAssigned"
+  tags = {
+    environment = "production"
   }
 }
 
-resource "azurerm_storage_account_customer_managed_key" "managed_key_good" {
-  storage_account_id = azurerm_storage_account.storage_account_good_1.id
-  key_vault_id       = azurerm_key_vault.example.id
-  key_name           = azurerm_key_vault_key.example.name
-  key_version = "1"
+resource "azurerm_key_vault" "example" {
+  name                        = "your-key-vault-name"
+  resource_group_name         = azurerm_resource_group.rg.name
+  location                    = azurerm_resource_group.rg.location
+  sku_name                    = "standard"
+  tenant_id                   = data.azuread_tenant.current.id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  enabled_for_disk_encryption = true
 }
 
 # Create virtual machine
